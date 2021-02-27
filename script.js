0f7f2a43a6aab4b8a39de97aa88a40b429910fd8
Marivaldo Sena <marivaldo.sena@live.com>
2021-02-27 12:23:14 -0300

    Make snake move.

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+const RIGHT = "right";
+const LEFT = "left";
+const UP = "up";
+const DOWN = "down";
+
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let direction = RIGHT;
 let snake = [];
 snake[0] = {
     x: 8 * box,
@@ -19,7 +25,59 @@ function createSnake() {
     }
 }
 
-window.addEventListener("DOMContentLoaded", function() {
+function update(event) {
+    if (event.keyCode === 37 && direction !== RIGHT) {
+        direction = LEFT;
+    }
+
+    if (event.keyCode === 38 && direction !== DOWN) {
+        direction = UP;
+    }
+
+    if (event.keyCode === 39 && direction !== LEFT) {
+        direction = RIGHT
+    }
+
+    if (event.keyCode === 40 && direction !== UP) {
+        direction = DOWN;
+    }
+}
+
+function startGame() {
     createBG();
     createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    switch (direction) {
+        case RIGHT:
+            snakeX += box;
+            break;
+        case LEFT:
+            snakeX -= box;
+            break;
+        case UP:
+            snakeY -= box;
+            break;
+        case DOWN:
+            snakeY += box;
+            break;
+        default: break;
+    }
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    let game = setInterval(startGame, 100);
 });
+
+document.addEventListener("keydown", update);
